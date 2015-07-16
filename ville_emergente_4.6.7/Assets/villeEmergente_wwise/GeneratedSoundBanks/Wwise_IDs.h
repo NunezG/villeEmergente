94cf3c7ee241652f0b26ff5dd5ca83398470216f
AMagenta <contact@amagenta.fr>
2015-07-16 15:50:35 +0200

    Premiers events

--- ville_emergente_4.6.7/Assets/villeEmergente_wwise/GeneratedSoundBanks/Wwise_IDs.h
+++ ville_emergente_4.6.7/Assets/villeEmergente_wwise/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,11 +13,32 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID FOOTSTEPS = 2385628198U;
+        static const AkUniqueID LACHER_MORCEAU = 2535466649U;
+        static const AkUniqueID LINKER_MORCEAU = 3679869857U;
+        static const AkUniqueID PRENDRE_MORCEAU = 2138522384U;
         static const AkUniqueID RAIN_2DOBJECT_PLAY = 812837510U;
         static const AkUniqueID RAIN_2DOBJECT_STOP = 2622262392U;
+        static const AkUniqueID TOUCHER_ELEMENT = 2896867782U;
+        static const AkUniqueID VILLE_CALME = 3115394940U;
         static const AkUniqueID WATER_3DOBJECT_PLAY = 193498114U;
         static const AkUniqueID WATER_3DOBJECT_STOP = 836511068U;
     } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace MATERIAL
+        {
+            static const AkUniqueID GROUP = 3865314626U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID CONCRETE = 841620460U;
+                static const AkUniqueID GRASS = 4248645337U;
+            } // namespace SWITCH
+        } // namespace MATERIAL
+
+    } // namespace SWITCHES
 
     namespace BANKS
     {
