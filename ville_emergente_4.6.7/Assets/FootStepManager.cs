3f4f61a1d8613f76e619ef237d4b385b28b2d72b
Plemar <aldebaranaintbadtoo@gmail.com>
2015-07-20 14:19:23 +0200

    intégration bruit de pas

---
+++ ville_emergente_4.6.7/Assets/FootStepManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FootStepManager : MonoBehaviour {
+
+    public CharacterController controller;
+    public bool isWalking=false, stoppedWalking=false;
+    public float isWalkingThreshold = 1;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (controller.isGrounded && controller.velocity.magnitude > isWalkingThreshold) // si le joueur est sur le sol et a une vitesse supérieur au seuil
+        {
+            isWalking = true; // alors c'est qu'il marche
+        }
+        else if (isWalking && (!controller.isGrounded || controller.velocity.magnitude < isWalkingThreshold)) // si le joueur était en train de marcher et qu'il n'est pas au sol, ou a une vitesse inférieur au seuil
+        {
+            isWalking=false;
+            stoppedWalking = true; // alors c'est qu'il s'est arrêté de marcher
+        }
+
+        if (isWalking)
+        {
+            WwiseAudioManager.instance.PlayLoopEvent("footsteps", this.gameObject);
+        }
+        if(stoppedWalking)
+        {
+            WwiseAudioManager.instance.StopLoopEvent("footsteps", this.gameObject);
+        }
+	}
+}
