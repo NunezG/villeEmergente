09c016d8529e347e761397b4b4ade82f5e316338
unknown <Guillermo.nunez@JMIN-D1422.formation.enjmin.fr>
2015-07-20 13:20:19 +0200

    scripts pour select de target

---
+++ ville_emergente_4.6.7/Assets/navigationScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class navigationScript : MonoBehaviour {
+
+	public List<GameObject> targets=null;
+
+
+
+	// Use this for initialization
+	void Start () {
+
+
+		GameObject[] targetList = GameObject.FindGameObjectsWithTag("NavigationTarget");
+		int i = 0;
+
+		while (i< 3)
+		{
+			int rand = Random.Range(0,targetList.Length-1);
+
+
+			if (!targetList[rand].Equals(this.gameObject) && !targets.Contains(targetList[rand]))
+			{
+				targets.Add (targetList[rand]);
+				i++;
+			}
+
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
