161e32cd3c428ff519967b829563a8a4ba238b26
unknown <Guillermo.nunez@JMIN-D1422.formation.enjmin.fr>
2015-07-21 14:28:40 +0200

    élimine code

--- ville_emergente_4.6.7/Assets/_Scripts/Building.cs
+++ ville_emergente_4.6.7/Assets/_Scripts/Building.cs
@@ -6,26 +6,6 @@ public class Building : InteractibleObject {
 	private float lastPos = 0;
 
 	public float collapsingSpeed = 10.0f;
-	 
-
-
-	// Use this for initialization
-	void Start () {
-		//lastPos = transform.position.y + 500.0f;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-		//transform.position = Vector3.Lerp(transform.position,new Vector3(transform.position.x, lastPos , transform.position.z), 888888.0f);
-
-
-
-		//transform.Translate(spawn.transform.position.x, GetComponent<Collider>().bounds.extents.y, spawn.transform.position.z);
-
-		//transform.localScale.
-
-	}
 
 	void OnMouseDown()
 	{
