d333f70a5a9b4cb24eb83bdefb34a1cb6026a83d
unknown <Guillermo.nunez@JMIN-D1422.formation.enjmin.fr>
2015-08-27 12:34:03 +0200

    Sound universe

---
+++ ville_emergente_4.6.7/Assets/_Scripts/SoundUniverseManager.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundUniverseManager : MonoBehaviour {
+	
+	private string switchAtmo = "switch_atmo";
+	public static string switchType;
+	private string switchDark = "switch_dark";
+
+	private float timer;
+
+	private float moveTimer;
+
+
+	private float stopTimer;
+	private int stopCounter;
+
+
+	// Use this for initialization
+	void Start () {
+		switchType = switchAtmo;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		timer += Time.deltaTime;
+
+		if (timer >= 120.0f) {
+			stopCounter = 0;
+		}
+
+		if (Input.GetKeyDown (KeyCode.Space))
+		{
+			if (switchType != switchDark)
+				switchType = switchDark;
+			 	
+		} else
+		if (Input.GetKey (KeyCode.Z) || Input.GetKey (KeyCode.Q) || Input.GetKey (KeyCode.S) || Input.GetKey (KeyCode.D)) {
+			moveTimer += Time.deltaTime;
+
+			stopTimer = 0;
+
+			if(moveTimer >=6.0f)
+			{
+				switchType = switchDark;
+			}
+
+			//resetTimer ();
+			
+		} else 
+		{
+			moveTimer = 0;
+			stopTimer += Time.deltaTime;
+
+			if (stopTimer >= 3.0f)
+			{
+				stopCounter++;
+				stopTimer = 0;
+			}
+
+			if (stopTimer >= 8.0f || stopCounter == 5) {
+				switchType = switchAtmo;
+			}
+		}
+	}
+
+	void resetTimer()
+	{
+		stopTimer = 0;
+		timer = 0;
+	}
+
+}
