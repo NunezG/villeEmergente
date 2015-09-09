1ee981dae8b28a83434f88b80d918a304570efaf
unknown <Maxime.lidolff@JMIN-D1438.formation.enjmin.fr>
2015-09-09 13:49:56 +0200

    Lvl design global

---
+++ ville_emergente_4.6.7/Assets/haut_bas.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class haut_bas : MonoBehaviour {
+
+    public float min;
+    public float max;
+
+    public float time;
+
+    private Vector3 pos;
+
+    private bool fall = true;
+
+    
+
+	// Use this for initialization
+	void Start () {
+
+        pos = transform.position;
+        pos.y = max;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (fall)
+        {
+            pos.y -= (max - min) * Time.deltaTime / time;
+
+            if (pos.y <= min)
+                fall = false;
+        }
+
+        else
+        {
+            pos.y += (max - min) *  Time.deltaTime / time;
+
+            if (pos.y >= max)
+                fall = true;
+        }
+
+        transform.position = pos;
+	}
+
+}
