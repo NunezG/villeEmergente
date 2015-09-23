f4cfe83d0f6aa7638188f2c0870077eb243938fb
Plemar <aldebaranaintbadtoo@gmail.com>
2015-09-23 10:09:07 +0200

    Correction Guide et point de vue

--- ville_emergente_4.6.7/Assets/AI/Actions/GuideSelectCompletedTarget.cs
+++ ville_emergente_4.6.7/Assets/AI/Actions/GuideSelectCompletedTarget.cs
@@ -12,7 +12,7 @@ public class GuideSelectCompletedTarget : RAINAction
     {
         base.Start(ai);
         target = ai.WorkingMemory.GetItem<GameObject>("target");
-        Debug.Log(ai.Body.name + " : SELECT TARGET ");
+        //Debug.Log(ai.Body.name + " : SELECT TARGET ");
 
 
     }
@@ -22,16 +22,24 @@ public class GuideSelectCompletedTarget : RAINAction
         List<GameObject> completedTargets = new List<GameObject>();
         for (int i = 0; i < ai.Body.GetComponent<Guide>().targets.Count; i++)
         {
-            if (ai.Body.GetComponent<Guide>().targets[i].GetComponent<PointDeVue>().batimentAVisiter.GetComponent<ConvolutionObject>().fragment != null)
+            if (ai.Body.GetComponent<Guide>().targets[i].GetComponent<PointDeVue>().batimentAVisiter.GetComponent<ConvolutionObject>().fragment != null // si le batiment a un fragment
+                && ai.Body.GetComponent<Guide>().targets[i].GetComponent<PointDeVue>().isBeingVisited == false) // et qu'un autre guide n'est pas en train de le visiter
             {
-                completedTargets.Add(ai.Body.GetComponent<Guide>().targets[i]); // on remplit la liste de tous les points de vue liés à des bâtiments complétés
+                completedTargets.Add(ai.Body.GetComponent<Guide>().targets[i]); // on l'ajoute à la liste
             }
         }
-        int targetIndex = Random.Range(0, completedTargets.Count); // on en prend un au hasard
-        target = completedTargets[targetIndex];
-        ai.WorkingMemory.SetItem<bool>("destinationReached", false);
+        if (ai.Body.GetComponent<Guide>().pdv != null)// si on était sur un point de vue
+            ai.Body.GetComponent<Guide>().pdv.isBeingVisited = false; // on le libere 
+
+        int targetIndex = Random.Range(0, completedTargets.Count); //puis on en prend un au hasard parmi ceux selectionnés
+
+        target = completedTargets[targetIndex]; // on l'ajoute en tant que destination
+
+        target.GetComponent<PointDeVue>().isBeingVisited = true; // on le réserve 
+
+        ai.WorkingMemory.SetItem<bool>("destinationReached", false); // on débloque la branche mouvement dans le BT
         ai.WorkingMemory.SetItem<GameObject>("target", target);
-        ai.Body.GetComponent<Guide>().pdv = ai.WorkingMemory.GetItem<GameObject>("target").GetComponent<PointDeVue>();
+        ai.Body.GetComponent<Guide>().pdv = ai.WorkingMemory.GetItem<GameObject>("target").GetComponent<PointDeVue>(); // on assigne le nouveau point de vue au guide
         return ActionResult.SUCCESS;
     }
 
