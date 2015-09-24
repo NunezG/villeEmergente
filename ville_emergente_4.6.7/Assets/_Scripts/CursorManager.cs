0a5f0fc836d5f4ee8b5e27b70fb05ceba295befc
unknown <Guillermo.nunez@JMIN-D1422.formation.enjmin.fr>
2015-09-24 17:46:30 +0200

    Correction erreurs de l'Interactor et optimisation du code

--- ville_emergente_4.6.7/Assets/_Scripts/CursorManager.cs
+++ ville_emergente_4.6.7/Assets/_Scripts/CursorManager.cs
@@ -7,7 +7,7 @@ public class CursorManager : MonoBehaviour {
 	public Sprite interactibleCursor;
 	public Sprite failCursor;
 	public Sprite fragmentCursor;
-
+	public CursorType cursorT = CursorType.normal;
 
 
 	// Use this for initialization
@@ -24,26 +24,29 @@ public class CursorManager : MonoBehaviour {
 
 	public void setNormalCursor()
 	{
+		cursorT = CursorType.normal;
 		GetComponent<UnityEngine.UI.Image> ().sprite = normalCursor;
 
 	}
 
 	public void setInteractibleCursor()
 	{
+		cursorT = CursorType.interactible;
 		GetComponent<UnityEngine.UI.Image> ().sprite = interactibleCursor;
 		
 	}
 
 	public void setFailCursor()
 	{
+		cursorT = CursorType.fail;
 		GetComponent<UnityEngine.UI.Image> ().sprite = failCursor;
 		
 	}
 
 	public void setFragmentCursor()
 	{
+		cursorT = CursorType.fragment;
 		GetComponent<UnityEngine.UI.Image> ().sprite = fragmentCursor;
-		
 	}
 
 
