9834b9f595d4154f6d1b9ded6f5b99720962dcd6
unknown <Guillermo.nunez@JMIN-D1422.formation.enjmin.fr>
2015-09-24 16:10:11 +0200

    correction danse du musicien

--- ville_emergente_4.6.7/Assets/_Scripts/AudioEventManager.cs
+++ ville_emergente_4.6.7/Assets/_Scripts/AudioEventManager.cs
@@ -93,7 +93,7 @@ public class AudioEventManager : MonoBehaviour {
 
 	public void soundDanse()
 	{
-		PlayFiniteEventWithCallBack (audioName.ToString()+"_danse");	
+		PlayFiniteEventWithCallBack (audioName.ToString()+"_joie");	
 	}
 
     
