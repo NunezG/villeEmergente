1bce4f2ddb54709a909e59a4c72dd33cb9f75d5d
unknown <Guillermo.nunez@JMIN-D1422.formation.enjmin.fr>
2015-09-25 16:57:28 +0200

    Changement de couleurs lors du swith atmo/dark

--- ville_emergente_4.6.7/Assets/_Scripts/SoundUniverseManager.cs
+++ ville_emergente_4.6.7/Assets/_Scripts/SoundUniverseManager.cs
@@ -6,7 +6,7 @@ public class SoundUniverseManager : MonoBehaviour {
 	
 	private string switchAtmo = "switch_atmo";
 	public static string switchType;
-	private string switchDark = "switch_atmo";
+	private string switchDark = "switch_dark";
 
 	public static float timer;
 	public static float moveTimer;
@@ -18,6 +18,25 @@ public class SoundUniverseManager : MonoBehaviour {
 
 	public static List<GameObject> playingObjects;
 
+	public Color ambientLight;
+	public Color upLightLight;
+	public Color downLightLight;
+	public Color skyBoxLight;
+
+	public Color ambientDark;
+	public Color upLightDark;
+	public Color downLightDark;
+	public Color skyBoxDark;
+
+
+	//public Color ambientTest;
+	//public Color upLightTest;
+	//public Color downLighTest;
+	//public Color skyBoxTest;
+
+
+	public float speed = 5f;
+
 	void Awake () {
 		playingObjects = new List<GameObject>();
 		switchType = switchAtmo;
@@ -28,10 +47,28 @@ public class SoundUniverseManager : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		WwiseAudioManager.PlayFiniteEvent("ville_calme", this.gameObject);
+
+
+		ambientLight = RenderSettings.ambientLight;
+		upLightLight = GameObject.Find ("Lumières").transform.FindChild ("Directional_light_up").GetComponent<Light> ().color;
+		downLightLight = GameObject.Find("Lumières").transform.FindChild("Directional_light_down").GetComponent<Light>().color;
+		skyBoxLight = RenderSettings.skybox.GetColor ("_Tint");
+		
+		ambientDark = new Color(64.0f/255.0f, 64.0F/255.0f, 64.0F/255.0f, 0.5F);
+		upLightDark = new Color(16.0F/255.0f, 30.0F/255.0f, 98.0F/255.0f, 0.5F);
+		downLightDark = new Color(141.0f/255.0f, 189.0F/255.0f, 254.0F/255.0f, 0.5F);
+		skyBoxDark = new Color(91.0F/255.0f, 77.0F/255.0f, 105.0F/255.0f, 0.5F);
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		//ambientTest = RenderSettings.ambientLight;
+		//upLightTest = GameObject.Find ("Lumières").transform.FindChild ("Directional_light_up").GetComponent<Light> ().color;
+		//downLighTest = GameObject.Find("Lumières").transform.FindChild("Directional_light_down").GetComponent<Light>().color;
+		//skyBoxTest = RenderSettings.skybox.GetColor ("_Tint");
+
 		switchTypeTest = switchType;
 
 		timer += Time.deltaTime;
@@ -61,6 +98,7 @@ public class SoundUniverseManager : MonoBehaviour {
 			{
 				switchType = switchDark;
 				switchSounds();
+				StartCoroutine("changeColors");
 			}
 
 			//resetTimer ();
@@ -75,6 +113,7 @@ public class SoundUniverseManager : MonoBehaviour {
 			{
 				switchType = switchAtmo;
 				switchSounds();
+				StartCoroutine("changeColors");
 			}
 
 			if (stopTimer >= 3.0f)
@@ -109,8 +148,43 @@ public class SoundUniverseManager : MonoBehaviour {
 		//soundsList.Remove (name, target);
 	//}
 
+
+	IEnumerator changeColors()
+	{
+
+		if (switchType != switchDark) {
+
+			while (GameObject.Find ("Lumières").transform.FindChild ("Directional_light_up").GetComponent<Light> ().color != upLightLight) {
+				
+				RenderSettings.ambientLight = Color.Lerp (RenderSettings.ambientLight, ambientLight, Time.deltaTime * speed);
+				GameObject.Find ("Lumières").transform.FindChild ("Directional_light_up").GetComponent<Light> ().color = Color.Lerp (GameObject.Find ("Lumières").transform.FindChild ("Directional_light_up").GetComponent<Light> ().color, upLightLight, Time.deltaTime * speed);
+				GameObject.Find ("Lumières").transform.FindChild ("Directional_light_down").GetComponent<Light> ().color = Color.Lerp (GameObject.Find ("Lumières").transform.FindChild ("Directional_light_down").GetComponent<Light> ().color, downLightLight, Time.deltaTime * speed);
+				RenderSettings.skybox.SetColor ("_Tint", Color.Lerp (RenderSettings.skybox.GetColor ("_Tint"), skyBoxLight, Time.deltaTime * speed));
+				
+				yield return new WaitForSeconds (.1f);
+			}
+
+
+
+		} else {
+
+
+			while (GameObject.Find ("Lumières").transform.FindChild ("Directional_light_up").GetComponent<Light> ().color != upLightDark) {
+
+				RenderSettings.ambientLight = Color.Lerp (RenderSettings.ambientLight, ambientDark, Time.deltaTime * speed);
+				GameObject.Find ("Lumières").transform.FindChild ("Directional_light_up").GetComponent<Light> ().color = Color.Lerp (GameObject.Find ("Lumières").transform.FindChild ("Directional_light_up").GetComponent<Light> ().color, upLightDark, Time.deltaTime * speed);
+				GameObject.Find ("Lumières").transform.FindChild ("Directional_light_down").GetComponent<Light> ().color = Color.Lerp (GameObject.Find ("Lumières").transform.FindChild ("Directional_light_down").GetComponent<Light> ().color, downLightDark, Time.deltaTime * speed);
+				RenderSettings.skybox.SetColor ("_Tint", Color.Lerp (RenderSettings.skybox.GetColor ("_Tint"), skyBoxDark, Time.deltaTime * speed));
+			
+				yield return new WaitForSeconds (.1f);
+			}
+		}
+	}
+
+	
 	static void switchSounds()
 	{
+
 	//	bool convolve = true;
 		//if (switchType == switchAtmo)
 	//		convolve = false;
